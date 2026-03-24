9bd84581b8371d0106de48a0f49ea537c92bc0ac
WillianCosta22 <willian.costa@ba.estudante.senai.br>
2026-03-24 15:17:57 -0300

    Migrando banco SQLite para Postgree

--- robot_sap/settings.py
+++ robot_sap/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-robot-sap-change-in-production-xyz123'
@@ -52,11 +53,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'robot_sap.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
