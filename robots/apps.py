f20dec9cb48f50dc00ec32090772938afd3e908b
WillianCosta22 <willian.costa@ba.estudante.senai.br>
2026-03-26 10:25:50 -0300

    Adicionado no apps.py

--- robots/apps.py
+++ robots/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 class RobotsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'robots'
+
+
+class RobotSapConfig(AppConfig):
+    name = 'robot_sap'
+
+    def ready(self):
+        from .init_superuser import create_superuser
+        create_superuser()
+
